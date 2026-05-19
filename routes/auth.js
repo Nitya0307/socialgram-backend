a7a7311f91c85eba616c8ab0e696914dcb44e400
Nitya <nitya@firsteconomy.com>
2026-05-19 13:52:12 +0530

    feat: upadte backend

---
+++ routes/auth.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const pool = require("../db");
+
+const router = express.Router();
+
+
+// SIGNUP
+router.post("/signup", async (req, res) => {
+
+  try {
+
+    const { username, email, mobile, password } = req.body;
+
+    // CHECK EMAIL
+    const existingUser = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
+
+    // HASH PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // INSERT USER
+    const newUser = await pool.query(
+      `INSERT INTO users
+      (username, email, mobile, password)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id, username, email, mobile`,
+      [username, email, mobile, hashedPassword]
+    );
+
+    // JWT TOKEN
+    const token = jwt.sign(
+      { id: newUser.rows[0].id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.status(201).json({
+      message: "Signup successful",
+      token,
+      user: newUser.rows[0],
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
+
+
+// LOGIN
+router.post("/login", async (req, res) => {
+
+  try {
+
+    const { email, password } = req.body;
+
+    // FIND USER
+    const user = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(400).json({
+        message: "Invalid email",
+      });
+    }
+
+    // CHECK PASSWORD
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].password
+    );
+
+    if (!validPassword) {
+      return res.status(400).json({
+        message: "Invalid password",
+      });
+    }
+
+    // CREATE TOKEN
+    const token = jwt.sign(
+      { id: user.rows[0].id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.rows[0].id,
+        username: user.rows[0].username,
+        email: user.rows[0].email,
+        mobile: user.rows[0].mobile,
+      },
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
+
+module.exports = router;
